6334dd71825f39ddeaf1c09a3915cafb0d26bdda
monicaivan25 <monicaivan25@gmail.com>
2019-08-23 13:46:14 +0300

    Finished implementing AnimationDrawable module

---
+++ app/src/main/java/com/monicaivan/animationdemo/DrawableAnimationActivity.java
@@ -0,0 +1,51 @@
+package com.monicaivan.animationdemo;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrawableAnimationActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    boolean started = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_animation);
+
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        ImageView imageView = findViewById(R.id.smiley_icon);
+        imageView.setBackgroundResource(R.drawable.my_custom_animation);
+        animationDrawable = (AnimationDrawable) imageView.getBackground();
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!started){
+                    animationDrawable.start();
+                    started = true;
+                } else {
+                    animationDrawable.stop();
+                    started = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/monicaivan/animationdemo/MainActivity.java
+++ app/src/main/java/com/monicaivan/animationdemo/MainActivity.java
@@ -2,13 +2,28 @@ package com.monicaivan.animationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button drawableAnimationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawableAnimationButton = findViewById(R.id.drawable_anim_button);
+
+
+        drawableAnimationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DrawableAnimationActivity.class));
+            }
+        });
     }
 }
